f41db17fc98640d0b740ad6297b40399310a74da
xiaodingfeng <1440651649@qq.com>
2020-04-27 18:20:19 +0800

    提交文件

--- app/src/main/java/com/example/xiaobai/yynote/ui/wait_main.java
+++ app/src/main/java/com/example/xiaobai/yynote/ui/wait_main.java
@@ -15,7 +15,6 @@ import com.example.xiaobai.yynote.R;
 public class wait_main extends Activity {
     private Handler handler;
     private Runnable runnable;
-    private int REQUEST_PERMISSION_CODE;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +24,6 @@ public class wait_main extends Activity {
         /**标题是属于View的，所以窗口所有的修饰部分被隐藏后标题依然有效,需要去掉标题**/
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_wait_main);
-        ActivityCompat.requestPermissions(wait_main.this,
-                new String[]{Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.INTERNET, Manifest.permission.RECORD_AUDIO,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.WAKE_LOCK,Manifest.permission.READ_EXTERNAL_STORAGE},
-                REQUEST_PERMISSION_CODE);
         handler = new Handler();
         runnable = new Runnable() {
             @Override
